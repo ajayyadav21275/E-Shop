a87b79967b58035bdc998140f71ed9a7f73e23be
Ajay kumar <ajayyadav21275@gmail.com>
2026-06-30 11:50:15 +0530

    change name of import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import Footer from "./Components/layout/Footer";
-import Navbar from "./Components/layout/Navbar";
-import AppRoutes from "./Routes/AppRoutes";
 
+import Footer from "./Components/layout/footer";
+import AppRoutes from "./Routes/AppRoutes";
+import Navbar from "./Components/layout/navbar";
 
 function App() {
   return (
     <>
-     <Navbar/>
+     <Navbar />
       <div className="container mt-3">
         <AppRoutes />
       </div>
